d06ffb0926a630631e93a09adb76d76e8850c7e3
Danil Ovchinnikov <danil42worker@gmail.com>
2022-06-10 18:28:40 +0300

    add run tests at gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    // Java support
-    id("java")
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.5.3"
+    id("org.jetbrains.intellij") version "1.6.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
 }
@@ -19,6 +17,10 @@ version = properties("pluginVersion")
 // Configure project's dependencies
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    testCompileOnly("junit:junit:4.13.2")
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
@@ -58,5 +60,19 @@ tasks {
                 getOrNull(properties("pluginVersion")) ?: getLatest()
             }.toHTML()
         })
+    }
+
+    test {
+        exclude("com/vk/kphpstorm/testing/infrastructure/**")
+        include("**/*Test.class")
+        isScanForTestClasses = false
+    }
+
+    sourceSets {
+        test {
+            resources {
+                srcDir("src/test/fixtures")
+            }
+        }
     }
 }
