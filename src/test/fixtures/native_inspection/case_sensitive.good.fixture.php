0b5bbdbbc51626dc5ed6d10d4701a1a70028e46b
Danil Ovchinnikov <Danil42Worker@gmail.com>
2022-08-09 01:29:33 +0700

    added tests

---
+++ src/test/fixtures/native_inspection/case_sensitive.good.fixture.php
@@ -0,0 +1,5 @@
+<?php
+
+class AbC {}
+
+$a = new <weak_warning descr="Case in class usage doesn't match the case in declaration">aBc</weak_warning>();
---
+++ src/test/kotlin/com/vk/kphpstorm/testing/tests/NativeInspectionTest.kt
@@ -0,0 +1,15 @@
+package com.vk.kphpstorm.testing.tests
+
+import com.jetbrains.php.lang.inspections.codeSmell.PhpMethodOrClassCallIsNotCaseSensitiveInspection
+import com.vk.kphpstorm.inspections.ArrayAndIndexingInspection
+import com.vk.kphpstorm.testing.infrastructure.InspectionTestBase
+
+class NativeInspectionTest : InspectionTestBase(ArrayAndIndexingInspection()) {
+
+    fun `test PhpMethodOrClassCallIsNotCaseSensitive inspection`() {
+        myFixture.enableInspections(PhpMethodOrClassCallIsNotCaseSensitiveInspection())
+
+        runFixture("native_inspection/case_sensitive.good.fixture.php")
+    }
+
+}
