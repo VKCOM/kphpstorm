244a3e91ccd58ca0c9f92174c025d32efe9ed135
Richardas Kuchinskas <r.kuchinskas@vk.team>
2025-06-27 01:19:51 +0300

    kphp tag template replaced

---
+++ src/main/kotlin/com/vk/kphpstorm/kphptags/psi/factory/KphpDocTagTemplateClassElementTypeFactory.kt
@@ -0,0 +1,25 @@
+package com.vk.kphpstorm.kphptags.psi.factory
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.StubElement
+import com.intellij.psi.stubs.StubElementFactory
+import com.jetbrains.php.lang.documentation.phpdoc.psi.stubs.PhpDocTagStub
+import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag
+import com.vk.kphpstorm.kphptags.psi.KphpDocElementTypes
+import com.vk.kphpstorm.kphptags.psi.serializers.KphpDocTagTemplateClassElementTypeSerializer
+
+object KphpDocTagTemplateClassElementTypeFactory : StubElementFactory<PhpDocTagStub, PhpDocTag> {
+    override fun createPsi(stub: PhpDocTagStub): PhpDocTag {
+        return KphpDocTagTemplateClassElementTypeSerializer().createPsi(stub)
+    }
+
+    override fun createStub(psi: PhpDocTag, parentStub: StubElement<out PsiElement>?): PhpDocTagStub {
+        return KphpDocTagTemplateClassElementTypeSerializer().createStub(psi, parentStub)
+    }
+
+    override fun shouldCreateStub(node: ASTNode): Boolean =
+        node.elementType == KphpDocElementTypes.kphpDocTagTemplateClass
+
+
+}
