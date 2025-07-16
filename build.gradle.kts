b25131482b1bf487b26f049472a6ad4209f682d2
Richardas Kuchinskas <r.kuchinskas@vk.team>
2025-07-16 16:56:30 +0300

    good

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,9 +35,10 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        // TODO: danil42russia think about it man
-//        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
-
+        create(
+            providers.gradleProperty("platformType"),
+            providers.gradleProperty("platformVersion")
+        )
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
 
