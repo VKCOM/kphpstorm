aa3a08d806b4daba788066cdd85997fcce704d57
vldf <vladislavf7@gmail.com>
2025-07-28 17:27:46 +0300

    remove redundant code

--- src/main/kotlin/com/vk/kphpstorm/kphptags/psi/stubs/KphpDocTagTemplateClassElementTypeFactory.kt
+++ src/main/kotlin/com/vk/kphpstorm/kphptags/psi/stubs/KphpDocTagTemplateClassElementTypeFactory.kt
@@ -1,15 +1,17 @@
-package com.vk.kphpstorm.kphptags.psi.factory
+package com.vk.kphpstorm.kphptags.psi.stubs
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.psi.stubs.StubElementFactory
+import com.intellij.util.io.StringRef
 import com.jetbrains.php.lang.documentation.phpdoc.psi.stubs.PhpDocTagStub
+import com.jetbrains.php.lang.documentation.phpdoc.psi.stubs.PhpDocTagStubImpl
 import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag
 import com.vk.kphpstorm.kphptags.psi.KphpDocElementTypes
-import com.vk.kphpstorm.kphptags.psi.KphpDocTagStubImpl
 import com.vk.kphpstorm.kphptags.psi.KphpDocTagTemplateClassPsiImpl
 
+@Suppress("UnstableApiUsage")
 object KphpDocTagTemplateClassElementTypeFactory : StubElementFactory<PhpDocTagStub, PhpDocTag> {
     override fun createPsi(stub: PhpDocTagStub): PhpDocTag {
         return KphpDocTagTemplateClassPsiImpl(stub, KphpDocElementTypes.kphpDocTagTemplateClass)
@@ -18,7 +20,13 @@ object KphpDocTagTemplateClassElementTypeFactory : StubElementFactory<PhpDocTagS
     override fun createStub(psi: PhpDocTag, parentStub: StubElement<out PsiElement>?): PhpDocTagStub {
         // stub value is 'T1,T2' — without spaces
         val stubValue = (psi as KphpDocTagTemplateClassPsiImpl).getTemplateArguments().joinToString(",")
-        return KphpDocTagStubImpl(parentStub, KphpDocElementTypes.kphpDocTagTemplateClass, psi.name, stubValue)
+
+        return PhpDocTagStubImpl(
+            parentStub,
+            KphpDocElementTypes.kphpDocTagTemplateClass,
+            StringRef.fromString(psi.name),
+            StringRef.fromString(stubValue)
+        )
     }
 
     override fun shouldCreateStub(node: ASTNode): Boolean =
