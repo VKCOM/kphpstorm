441a58edecdf6b6388b92e2013b831cc26eea24e
vldf <vladislavf7@gmail.com>
2025-07-30 13:22:17 +0300

    fix kphp-json, kphp-template and kphp-warn-performance stubs

--- src/main/kotlin/com/vk/kphpstorm/kphptags/psi/KphpDocElementTypes.kt
+++ src/main/kotlin/com/vk/kphpstorm/kphptags/psi/KphpDocElementTypes.kt
@@ -22,7 +22,7 @@ object KphpDocElementTypes {
      * * do not store anything in stubs, so their contents is unaccessible from other files
      * Such doc tags are 'simple'
      */
-    val kphpDocTagSimple = PhpElementType("@kphp-simple")
+    val kphpDocTagSimple = PhpElementType("<kphp-simple-tag>")
 
     /**
      * '@kphp-warn-performance ...' and '@kphp-analyze-performance ...'
