f742cc55b8ecb43099672d7a68f6a18f5a5edd27
vldf <vladislavf7@gmail.com>
2025-07-30 17:26:51 +0300

    update tests

--- src/test/fixtures/kphp_stub/kphp-template.stub.php
+++ src/test/fixtures/kphp_stub/kphp-template.stub.php
@@ -1,4 +1,4 @@
 PhpFileStubImpl
   DOC_COMMENT:PhpDocCommentStubImpl
-    @kphp-template-class {name: @kphp-template-class, value: int,string}:@kphp-template-class: int,string
+    @kphp-template-class {name: @kphp-template-class, value: int,string}:PhpDocTagStubImpl
   CLASS:PhpClassStubImpl{C}
